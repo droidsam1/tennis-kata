31f93f7c53b9e68f95205fdfc93598257fd890e9
droidsam1 <droidsam@gmail.com>
2022-12-04 08:53:19 +0100

    refactor: remove player2points and use class propety

--- java/src/main/java/TennisGame2.java
+++ java/src/main/java/TennisGame2.java
@@ -3,8 +3,6 @@ public class TennisGame2 implements TennisGame {
     public final Player player1;
     public final Player player2;
 
-    public int player2Points = 0;
-
     public String player2Score = "";
 
     public TennisGame2(String player1Name, String player2Name) {
@@ -14,7 +12,7 @@ public class TennisGame2 implements TennisGame {
 
     public String getScore() {
         String score = "";
-        if (player1.points == player2Points && player1.points < 4) {
+        if (player1.points == player2.points && player1.points < 4) {
             if (player1.points == 0)
                 score = "Love";
             if (player1.points == 1)
@@ -23,10 +21,10 @@ public class TennisGame2 implements TennisGame {
                 score = "Thirty";
             score += "-All";
         }
-        if (player1.points == player2Points && player1.points >= 3)
+        if (player1.points == player2.points && player1.points >= 3)
             score = "Deuce";
 
-        if (player1.points > 0 && player2Points == 0) {
+        if (player1.points > 0 && player2.points == 0) {
             if (player1.points == 1)
                 player1.score = "Fifteen";
             if (player1.points == 2)
@@ -37,33 +35,33 @@ public class TennisGame2 implements TennisGame {
             player2Score = "Love";
             score = player1.score + "-" + player2Score;
         }
-        if (player2Points > 0 && player1.points == 0) {
-            if (player2Points == 1)
+        if (player2.points > 0 && player1.points == 0) {
+            if (player2.points == 1)
                 player2Score = "Fifteen";
-            if (player2Points == 2)
+            if (player2.points == 2)
                 player2Score = "Thirty";
-            if (player2Points == 3)
+            if (player2.points == 3)
                 player2Score = "Forty";
 
             player1.score = "Love";
             score = player1.score + "-" + player2Score;
         }
 
-        if (player1.points > player2Points && player1.points < 4) {
+        if (player1.points > player2.points && player1.points < 4) {
             if (player1.points == 2)
                 player1.score = "Thirty";
             if (player1.points == 3)
                 player1.score = "Forty";
-            if (player2Points == 1)
+            if (player2.points == 1)
                 player2Score = "Fifteen";
-            if (player2Points == 2)
+            if (player2.points == 2)
                 player2Score = "Thirty";
             score = player1.score + "-" + player2Score;
         }
-        if (player2Points > player1.points && player2Points < 4) {
-            if (player2Points == 2)
+        if (player2.points > player1.points && player2.points < 4) {
+            if (player2.points == 2)
                 player2Score = "Thirty";
-            if (player2Points == 3)
+            if (player2.points == 3)
                 player2Score = "Forty";
             if (player1.points == 1)
                 player1.score = "Fifteen";
@@ -72,31 +70,31 @@ public class TennisGame2 implements TennisGame {
             score = player1.score + "-" + player2Score;
         }
 
-        if (player1.points > player2Points && player2Points >= 3) {
+        if (player1.points > player2.points && player2.points >= 3) {
             score = "Advantage player1";
         }
 
-        if (player2Points > player1.points && player1.points >= 3) {
+        if (player2.points > player1.points && player1.points >= 3) {
             score = "Advantage player2";
         }
 
-        if (player1.points >= 4 && player2Points >= 0 && (player1.points - player2Points) >= 2) {
+        if (player1.points >= 4 && player2.points >= 0 && (player1.points - player2.points) >= 2) {
             score = "Win for player1";
         }
-        if (player2Points >= 4 && player1.points >= 0 && (player2Points - player1.points) >= 2) {
+        if (player2.points >= 4 && player1.points >= 0 && (player2.points - player1.points) >= 2) {
             score = "Win for player2";
         }
         return score;
     }
 
     public void incrementPlayer2Points() {
-        player2Points++;
+        player2.points++;
     }
 
     public void wonPoint(String player) {
         if (player1.name.equals(player))
             player1.incrementPlayer1Points();
         else
-            incrementPlayer2Points();
+            player2.incrementPlayer1Points();
     }
 }
