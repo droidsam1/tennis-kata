a1aa4b4142974796b7d9a3c41d8073db1e739bb0
droidsam1 <droidsam@gmail.com>
2022-12-06 11:32:56 +0100

    refactor: simplify condition

--- java/src/main/java/TennisGame3.java
+++ java/src/main/java/TennisGame3.java
@@ -15,7 +15,7 @@ public class TennisGame3 implements TennisGame {
     }
 
     public String getScore() {
-        if ((player1Points >= 4 || player2Points >= 4 || player1Points + player2Points == 6) && isATie()) {
+        if (isATie() && (player1Points >= 4 || player1Points + player2Points == 6)) {
             return "Deuce";
         }
         if (player1Points >= 4 || player2Points >= 4) {
